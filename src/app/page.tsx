503462adb1500fd7694efbb34b4e56e28b57c521
Стас Пивоваров <stas.povovarov@headmade.pro>
2024-07-01 19:22:04 +0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,140 @@
+"use client"
+
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "./page.module.scss";
+import { Col, Row } from "antd";
+import CodeMirror from '@uiw/react-codemirror';
+import { useState } from "react";
+import { RJSFSchema } from '@rjsf/utils';
+import Form from '@rjsf/antd';
+import validator from '@rjsf/validator-ajv8';
 
 export default function Home() {
+  const initialSchema = {
+    title: "A registration form",
+    description: "A simple form example.",
+    type: "object",
+    required: ["firstName", "lastName"],
+    properties: {
+      firstName: {
+        type: "string",
+        title: "First name",
+        default: "Chuck",
+      },
+      lastName: {
+        type: "string",
+        title: "Last name",
+      },
+      age: {
+        type: "integer",
+        title: "Age",
+      },
+      married: {
+        type: "boolean",
+        title: "Marriage Status"
+      }
+    },
+  };
+
+
+  const initialUiSchema = {
+    "firstName": {
+      "ui:autofocus": true,
+      "ui:emptyValue": "Chuck",
+      "ui:placeholder": "Enter your first name"
+    },
+    "lastName": {
+      "ui:widget": "text",
+      "ui:placeholder": "Enter your last name"
+    },
+    "age": {
+      "ui:widget": "updown",
+      "ui:title": "Age",
+      "ui:description": "Your age in years"
+    },
+    "married": {
+      "ui:widget": "radio",
+      "ui:title": "Marital Status",
+      "ui:options": {
+        "enumOptions": [
+          {
+            "label": "Yes",
+            "value": true
+          },
+          {
+            "label": "No",
+            "value": false
+          }
+        ]
+      }
+    }
+  };
+
+  const [rjsf, setRjsf] = useState<any>(initialSchema);
+  const [uiScheme, setUiScheme] = useState<any>(initialUiSchema);
+  const [body, setBody] = useState<any>()
+
+  const handleRjsfChange = (value: string) => {
+    try {
+      const parsedValue = JSON.parse(value);
+      setRjsf(parsedValue);
+    } catch (error) {
+      console.error("Invalid JSON for rjsf schema:", error);
+    }
+  };
+
+  const handleUiSchemeChange = (value: string) => {
+    try {
+      const parsedValue = JSON.parse(value);
+      setUiScheme(parsedValue);
+    } catch (error) {
+      console.error("Invalid JSON for uiScheme:", error);
+    }
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
+    <Row justify={'center'}>
+      <Col lg={20}>
+        <h1>RJSF Playground</h1>
+        <Row justify={'space-between'}>
+          <Col span={11}>
+            <Col span={24}>
+              <h2>RJSF Schema</h2>
+              <CodeMirror
+                value={JSON.stringify(rjsf, null, 2)}
+                height="200px"
+                onChange={(editor) => handleRjsfChange(editor)}
+              />
+            </Col>
+            <Col span={24}>
+              <h2>UI Schema</h2>
+              <CodeMirror
+                value={JSON.stringify(uiScheme, null, 2)}
+                height="200px"
+                onChange={(editor) => handleUiSchemeChange(editor)}
+              />
+            </Col>
+          </Col>
+          <Col span={11}>
+            <h2>Form</h2>
+            <Form
+              schema={rjsf}
+              uiSchema={uiScheme}
+              validator={validator}
+              onSubmit={(value) => setBody(value?.formData)}
             />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          </Col>
+        </Row>
+        <Row>
+        <Col span={24}>
+        <h2>Request Body</h2>
+        <CodeMirror
+                value={JSON.stringify(body, null, 2)}
+                height="200px"
+              />
+        </Col>
+      </Row>
+      </Col>
+    </Row>
   );
 }
